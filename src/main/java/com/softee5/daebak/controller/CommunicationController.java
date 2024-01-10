d42826fece292f256474a2f443f4c725357734f7
yo0919 <0919yo@naver.com>
2024-01-10 13:02:27 +0900

    fix : exceptionhandler 없어졌던 문제 해결

--- src/main/java/com/softee5/daebak/controller/CommunicationController.java
+++ src/main/java/com/softee5/daebak/controller/CommunicationController.java
@@ -27,5 +27,26 @@ public class CommunicationController {
         CommunicationCreateResponseDto responseDto = new CommunicationCreateResponseDto(true, 200, "소통 생성 성공");
         return ResponseEntity.ok(responseDto);
     }
+
+    // 서버 측 에러 예외처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<CommunicationCreateResponseDto> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new CommunicationCreateResponseDto(false, 500, "서버 측 에러"));
+    }
+
+    // 클라이언트 측(잘못된 요청) 에러 예외처리
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<CommunicationCreateResponseDto> handleBadRequestException(BadRequestException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new CommunicationCreateResponseDto(false, 400, "잘못된 요청입니다"));
+    }
+
+    // 자원을 찾을 수 없는 경우에 대한 예외처리
+    @ExceptionHandler(ChangeSetPersister.NotFoundException.class)
+    public ResponseEntity<CommunicationCreateResponseDto> handleNotFoundException(ChangeSetPersister.NotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new CommunicationCreateResponseDto(false, 404, "요청한 자원을 찾을 수 없습니다"));
+    }
 }
 
